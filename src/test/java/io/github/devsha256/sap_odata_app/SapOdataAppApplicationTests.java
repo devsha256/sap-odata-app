4fce0d79cf90d9466019ea82aee285184f6c25fd
devsha256 <dev.hsaddam@gmail.com>
2025-11-28 15:22:33 +0530

    Add sapOdata servies

---
+++ src/test/java/io/github/devsha256/sap_odata_app/SapOdataAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.devsha256.sap_odata_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SapOdataAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
